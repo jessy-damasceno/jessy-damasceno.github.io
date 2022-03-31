131f682f8554bbba243ac56cef8b401039f9454d
Jessy Damasceno <jessy_bas@hotmail.com>
2022-03-31 03:23:38 -0300

    add home landpage

---
+++ home/script.js
@@ -0,0 +1,50 @@
+const home = document.getElementById('home');
+const git = document.getElementById('github');
+const profile = document.getElementById('profile');
+const linkedin = document.getElementById('linkedin');
+const spanHome = document.getElementById('span-home');
+const spanGit = document.getElementById('span-git');
+const spanProfile = document.getElementById('span-profile');
+const spanLink = document.getElementById('span-linkedin');
+const spans = document.getElementsByTagName('span');
+console.log(spans);
+
+function homeColor() {
+  document.body.style.backgroundColor = 'rgb(229, 169, 54)';
+  spanHome.style.display = 'inline';
+}
+
+function gitColor() {
+  document.body.style.backgroundColor = 'rgba(30, 30, 30, 0.8)';
+  spanGit.style.display = 'inline';
+}
+
+function profileColor() {
+  document.body.style.backgroundColor = 'green';
+  spanProfile.style.display = 'inline';
+}
+
+function linkedinColor() {
+  document.body.style.backgroundColor = 'rgb(8, 120, 172)';
+  spanLink.style.display = 'inline';
+
+}
+
+function retornaCor() {
+  document.body.style.backgroundColor = 'rgb(229, 169, 54)';
+  for (let span of spans) {
+    span.style.display = 'none';
+  }
+}
+
+home.addEventListener('mouseover', homeColor);
+home.addEventListener('mouseout', retornaCor);
+
+git.addEventListener('mouseover', gitColor);
+git.addEventListener('mouseout', retornaCor);
+
+profile.addEventListener('mouseover', profileColor);
+profile.addEventListener('mouseout', retornaCor);
+
+linkedin.addEventListener('mouseover', linkedinColor);
+linkedin.addEventListener('mouseout', retornaCor);
