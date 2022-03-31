ab58e0f05e3672fe51e30bceb4b8c0acdcb5afa5
Jessy Damasceno <jessy_bas@hotmail.com>
2022-03-31 11:03:05 -0300

    finalizando page - por enquanto

--- home/script.js
+++ home/script.js
@@ -1,39 +1,47 @@
-const home = document.getElementById('home');
-const git = document.getElementById('github');
-const profile = document.getElementById('profile');
-const linkedin = document.getElementById('linkedin');
-const spanHome = document.getElementById('span-home');
-const spanGit = document.getElementById('span-git');
-const spanProfile = document.getElementById('span-profile');
-const spanLink = document.getElementById('span-linkedin');
-const spans = document.getElementsByTagName('span');
+const home = document.getElementById("home");
+const git = document.getElementById("github");
+const profile = document.getElementById("profile");
+const linkedin = document.getElementById("linkedin");
+const spanHome = document.getElementById("span-home");
+const spanGit = document.getElementById("span-git");
+const spanProfile = document.getElementById("span-profile");
+const spanLink = document.getElementById("span-linkedin");
+const spans = document.getElementsByTagName("span");
 
+var x = window.matchMedia("(min-width: 700px)");
 
 function homeColor() {
-  document.body.style.backgroundColor = 'rgb(229, 169, 54)';
-  spanHome.style.display = 'inline';
+  if (x.matches) {
+    document.body.style.backgroundColor = "rgb(229, 169, 54)";
+    spanHome.style.display = "inline";  
+  }
 }
 
 function gitColor() {
-  document.body.style.backgroundColor = 'rgba(30, 30, 30, 0.8)';
-  spanGit.style.display = 'inline';
+  if (x.matches) {
+    document.body.style.backgroundColor = "rgba(30, 30, 30, 0.8)";
+    spanGit.style.display = "inline";  
+  }
 }
 
 function profileColor() {
-  document.body.style.backgroundColor = 'green';
-  spanProfile.style.display = 'inline';
+  if (x.matches) {
+    document.body.style.backgroundColor = "green";
+    spanProfile.style.display = "inline";
+  }
 }
 
 function linkedinColor() {
-  document.body.style.backgroundColor = 'rgb(8, 120, 172)';
-  spanLink.style.display = 'inline';
-
+  if (x.matches) {
+    document.body.style.backgroundColor = "rgb(8, 120, 172)";
+    spanLink.style.display = "inline";
+  }
 }
 
 function retornaCor() {
-  document.body.style.backgroundColor = 'rgb(229, 169, 54)';
+  document.body.style.backgroundColor = "rgb(229, 169, 54)";
   for (let span of spans) {
-    span.style.display = 'none';
+    span.style.display = "none";
   }
 }
 
@@ -46,19 +54,15 @@ function wow() {
          ||     ||                `);
 }
 
-var x = window.matchMedia("(max-width: 700px)")
-
-if (x.matches) {
-  home.addEventListener('mouseover', homeColor);
-  home.addEventListener('click', wow)
-  home.addEventListener('mouseout', retornaCor);
+  home.addEventListener("mouseover", homeColor);
+  home.addEventListener("click", wow);
+  home.addEventListener("mouseout", retornaCor);
   
-  git.addEventListener('mouseover', gitColor);
-  git.addEventListener('mouseout', retornaCor);
+  git.addEventListener("mouseover", gitColor);
+  git.addEventListener("mouseout", retornaCor);
   
-  profile.addEventListener('mouseover', profileColor);
-  profile.addEventListener('mouseout', retornaCor);
+  profile.addEventListener("mouseover", profileColor);
+  profile.addEventListener("mouseout", retornaCor);
   
-  linkedin.addEventListener('mouseover', linkedinColor);
-  linkedin.addEventListener('mouseout', retornaCor);
-}
+  linkedin.addEventListener("mouseover", linkedinColor);
+  linkedin.addEventListener("mouseout", retornaCor);
